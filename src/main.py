5b4449e299d644ea53486c86856aa5f02cb5aa19
mstechly <michal.stechly@gmail.com>
2019-04-15 17:06:14 +0200

    Implemented cost operator creation and WIP clause simplification.

---
+++ src/main.py
@@ -0,0 +1,79 @@
+from preprocessing import create_problem_representation
+import pdb
+from grove.pyqaoa.qaoa import QAOA
+from pyquil.paulis import PauliTerm, PauliSum
+import pyquil.quil as pq
+from pyquil.gates import X, I
+
+
+def factor_number(m):
+    p_dict, q_dict, z_dict, clauses = create_problem_representation(m)
+    cost_operators, mapping = create_operators_from_clauses(clauses)
+    driver_operators = create_driver_operators(mapping)
+
+
+def create_operators_from_clauses(clauses):
+    operators = []
+    mapping = {}
+    variable_counter = 0
+    for clause in clauses:
+        if clause == 0:
+            continue
+        variables = list(clause.free_symbols)
+        for variable in variables:
+            mapping[str(variable)] = variable_counter
+            variable_counter += 1
+        pauli_terms = []
+        quadratic_pauli_terms = []
+        for single_term in clause.args:
+            if len(single_term.free_symbols) == 0:
+                print("Constant term", single_term)
+                pauli_terms.append(PauliTerm("I", 0, int(single_term) / 2))
+            elif len(single_term.free_symbols) == 1:
+                print("Single term", single_term)
+                symbol = list(single_term.free_symbols)[0]
+                symbol_id = mapping[str(symbol)]
+                pauli_terms.append(PauliTerm("I", symbol_id, 1/2))
+                pauli_terms.append(PauliTerm("Z", symbol_id, -1/2))
+            elif len(single_term.free_symbols) == 2:
+                print("Double term", single_term)
+                symbol_1 = list(single_term.free_symbols)[0]
+                symbol_2 = list(single_term.free_symbols)[1]
+                symbol_id_1 = mapping[str(symbol_1)]
+                symbol_id_2 = mapping[str(symbol_2)]
+                pauli_term_1 = PauliTerm("I", symbol_id_1, 1/2) - PauliTerm("Z", symbol_id_1, 1/2)
+                pauli_term_2 = PauliTerm("I", symbol_id_2, 1/2) - PauliTerm("Z", symbol_id_2, 1/2)
+                quadratic_pauli_terms.append(pauli_term_1 * pauli_term_2)
+            else:
+                print("Terms of orders higher than quadratic are not handled.")
+        clause_operator = PauliSum(pauli_terms)
+        for quadratic_term in quadratic_pauli_terms:
+            clause_operator += quadratic_term
+        squared_clause_operator = clause_operator*clause_operator
+        print("C:", clause_operator)
+        print("C**2:", squared_clause_operator)
+        operators.append(squared_clause_operator)
+
+
+    return operators, mapping
+
+def create_driver_operators(mapping):
+    driver_operators = []
+    
+    for key, value in mapping.items():
+        driver_operators.append(PauliSum([PauliTerm("X", value, -1.0)]))
+
+    return driver_operators
+
+
+def main():
+    m = 15
+    if m % 2 == 0:
+        p = 2
+        q = int(m / 2)
+        print("The primes are:", p,"and", q)
+    else:
+        factor_number(m)
+
+if __name__ == '__main__':
+    main()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,39 +1,54 @@
 import numpy as np
 import pdb
+import sympy
+from sympy import symbols, Eq, FiniteSet, solveset
 
-
-def create_problem_representation(m_int, preprocessing=True):
+def create_problem_representation(m_int, apply_preprocessing=True):
     """
     Creates clauses VQF algorithm.
     """
+
     m_dict, p_dict, q_dict, z_dict = create_initial_dicts(m_int)
-    p_dict, q_dict, z_dict = apply_preprocessing_rules(m_dict, p_dict, q_dict, z_dict)
+
+    # simplify_clauses(clauses)
+    if apply_preprocessing:
+        p_dict, q_dict, z_dict = apply_preprocessing_rules(m_dict, p_dict, q_dict, z_dict)
+
+    clauses = create_clauses(m_dict, p_dict, q_dict, z_dict)
+
+    return p_dict, q_dict, z_dict, clauses
 
 
 def create_initial_dicts(m_int):
-    m_binary = bin(m_int)
+    m_binary = bin(m_int)[2:][::-1]
 
     m_dict = {}
-    for i, item in enumerate(m_binary[2:][::-1]):
+    n_m = len(m_binary)
+    for i, item in enumerate(m_binary):
         m_dict[i] = int(item)
 
     p_dict = {}
     n_p = len(m_dict)
     for i in range(n_p):
-        p_dict[i] = np.nan
+        p_dict[i] = symbols('p_'+str(i))
 
     q_dict = {}
     n_q = int(np.ceil(len(m_dict)/2))
     for i in range(n_q):
-        q_dict[i] = np.nan
+        q_dict[i] = symbols('q_'+str(i))
 
     n_c = n_p + n_q - 1
 
     z_dict = {}
     for i in range(n_c):
         for j in range(i+1):
-            if j+1 == i:
-                z_dict[(j, i)] = np.nan
+            if i!=j:
+                if i >= n_m:
+                    z_dict[(j, i)] = 0
+                elif j==0:
+                    z_dict[(j, i)] = 0
+                else:
+                    z_dict[(j, i)] = symbols('z_'+str(j)+'_'+str(i))
             else:
                 z_dict[(j, i)] = 0
 
@@ -44,18 +59,35 @@ def apply_preprocessing_rules(m_dict, p_dict, q_dict, z_dict):
     #Comes from the assumption that p and q are odd. If they are even, the problem is trivial.
     p_dict[0] = 1
     q_dict[0] = 1
-    z_dict[(0, 1)] = 1
+    z_dict[(0, 1)] = 0
     return p_dict, q_dict, z_dict
 
 
-def main():
-    m = 15
-    if m % 2 == 0:
-        p = 2
-        q = int(m / 2)
-        print("The primes are:", p,"and", q)
-    else:
-        create_clauses(m)
+def create_clauses(m_dict, p_dict, q_dict, z_dict):
+    clauses = []
+    n_c = len(p_dict) + len(q_dict) - 1
+    for i in range(n_c):
+        clause = 0
+        for j in range(i+1):
+            clause += q_dict.get(j,0)*p_dict.get(i-j, 0)
+
+        for j in range(i+1):
+            clause += z_dict.get((j, i), 0)
+
+        clause += -m_dict.get(i, 0)
+
+        # for j in range(1, n_c):
+        #     clause += - 2**j * z_dict.get((i, i+j), 0)
+        clauses.append(clause)
+    return clauses
+
+
+def simplify_clauses(clauses):
+    solution_set = FiniteSet(0, 1)
+    for clause in clauses:
+        variables = list(clause.free_symbols)
+        solveset(Eq(clause, 0), variables, domain=solution_set)
+        #TODO
 
 
 if __name__ == '__main__':
