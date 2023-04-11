18ec6b1e15d817492ad29a894875c8e70239dd3a
Mostafa Atallah <m.ataallah.2014@gmail.com>
2023-04-12 00:20:10 +0200

    Making the package instalable

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vqf",
+    version="0.1.0",
+    description="This repository contains implementation of the algorithm presented in the article \"Variational Quantum Factoring\", by Eric R. Anschuetz, Jonathan P. Olson, Alán Aspuru-Guzik, Yudong Cao.",
+    author="Michal Stechly",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[
+        # add any package dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
